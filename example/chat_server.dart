07ed12360d3c7f8f7070e8fee00a7e1919c40e54
George Moschovitis <george.moschovitis@gmail.com>
2013-01-13 08:06:20 +0200

    Minor fixes.

--- example/chat_server.dart
+++ example/chat_server.dart
@@ -3,14 +3,14 @@ import 'package:wamp/wamp_server.dart';
 
 class ChatHandler extends WampHandler {
   void onCall(c, callId, uri, arg) {
-    c.callResult(callId, 'It fuckin works');
+    c.callResult(callId, 'RPC message accepted: $uri');
   }
 }
 
 void main() {
   HttpServer server = new HttpServer();
 
-  server.onError = (error) => print(error);
+//  server.onError = (error) => print(error);
   server.listen('127.0.0.1', 8080);
 
   WebSocketHandler wsHandler = new WebSocketHandler();
